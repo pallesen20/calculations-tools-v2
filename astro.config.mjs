5da6a7e890887bf8bc513605bb30a9a0c143b7dc
Jesper Pallesen <jesperpallesen@Mac.home>
2026-04-23 16:51:42 +0200

    redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,4 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   trailingSlash: 'never',
   site: 'https://calculations.tools',
-  redirects: {
-    '/percentage-calculator':                           '/math/percentage-calculator',
-    '/compare-text':                                    '/compare/text',
-    '/compare-text/compare-excel':                      '/compare/excel',
-  },
 });
